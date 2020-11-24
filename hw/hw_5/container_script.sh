f3b8c8e673b40689ed985ffc0fb62fffe8fcf612
my-cabbages <ibaumann.randb@gmail.com>
2020-11-23 19:09:27 -0800

    removing unecessary sh script
    committed old container_script.sh by accident, removing here

--- hw/hw_5/container_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mysql -h 172.19.0.2 -P 3307 baseball < baseball.sql
-#mysql < /scripts/running_avg.sql
-#echo "This is a test (2)"
-
-if [ -d /var/lib/mysql/baseball ] ; then 
-    # Do Stuff ...
-fi
